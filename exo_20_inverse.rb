948392811366646d6216b954dcc009d8a9e1c463
Maryliss <43214794+Maryliss@users.noreply.github.com>
2019-01-01 22:54:00 +0100

    Add files via upload

--- exo_20_inverse.rb
+++ exo_20_inverse.rb
@@ -4,11 +4,9 @@ if etage < 1
     puts "Ton chiffre doit être supérieur à 1"
 elsif etage > 25
     puts "Ton nombre doit être inférieur à 25"
-elsif etage <=25
-    n = 1
-    while n <= etage
-        x = etage-n
-        puts " " * x +("#" * n)
-        n += 1
-    end
+end
+
+while etage >= 1
+    puts "# " * etage
+    etage -= 1
 end
